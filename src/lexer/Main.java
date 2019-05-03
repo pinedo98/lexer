30bba5f223f01154deb51a14ec6de596d2b335fa
Alex Vilchis <alexrv4299@gmail.com>
2019-05-03 17:24:00 -0500

    Agrega espacios

--- src/lexer/Main.java
+++ src/lexer/Main.java
@@ -19,7 +19,6 @@ public class Main extends Application {
   public void start(Stage primaryStage) throws Exception {
     primaryStage.setTitle("Analizador Léxico");
     primaryStage.getIcons().add(new Image("file:src/lexer/analyze.png"));
-
     FXMLLoader fxmlLoader = new FXMLLoader(new URL(Main.class.getResource("MainView.fxml").toExternalForm()));
     Parent parent = fxmlLoader.load();
     primaryStage.setScene(new Scene(parent));
